e49004fb302841dcffa41d34006af2cc30af4fbf
Steve <steve.kirby@mailbox.org>
2020-01-08 09:20:42 +0100

    Korrektur

--- Printer/Add-PrinterDriver.ps1
+++ Printer/Add-PrinterDriver.ps1
@@ -1,3 +1,3 @@
-$PrinterServer=DDS01
+$PrintServer = "DDS01"
 
-$Wmi = ([wmiclass]'Win32_Printer') ; $Wmi.Scope.Options.EnablePrivileges = $true; gwmi win32_printer -ComputerName $PrinterServer -Filter 'shared=true' | foreach {$Wmi.AddPrinterConnection( [string]::Concat('\\', $_.__SERVER, '\', $_.ShareName) )}
+$Wmi = ([wmiclass]'Win32_Printer') ; $Wmi.Scope.Options.EnablePrivileges = $true; gwmi win32_printer -ComputerName $PrintServer -Filter 'shared=true' | foreach {$Wmi.AddPrinterConnection( [string]::Concat('\\', $_.__SERVER, '\', $_.ShareName) )}
