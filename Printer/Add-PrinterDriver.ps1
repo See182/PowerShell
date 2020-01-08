28835654d855f25c367910091c98fa02ee082ad3
Steve <steve.kirby@mailbox.org>
2020-01-08 09:23:46 +0100

    Script erweitert

--- Printer/Add-PrinterDriver.ps1
+++ Printer/Add-PrinterDriver.ps1
@@ -1,3 +1,31 @@
+# Install all Printers from the PrintServer
 $PrintServer = "DDS01"
 
 $Wmi = ([wmiclass]'Win32_Printer') ; $Wmi.Scope.Options.EnablePrivileges = $true; gwmi win32_printer -ComputerName $PrintServer -Filter 'shared=true' | foreach {$Wmi.AddPrinterConnection( [string]::Concat('\\', $_.__SERVER, '\', $_.ShareName) )}
+
+# Remove all installed network Printers
+Function Remove-OSCNetworkPrinters
+{
+	$NetworkPrinters = Get-WmiObject -Class Win32_Printer | Where-Object{$_.Network}
+	If ($null -ne $NetworkPrinters)
+	{
+		Try
+		{
+			Foreach($NetworkPrinter in $NetworkPrinters)
+			{
+				$NetworkPrinter.Delete()
+				Write-Host "Successfully deleted the network printer:" + $NetworkPrinter.Name -ForegroundColor Green	
+			}
+		}
+		Catch
+		{
+			Write-Host $_
+		}
+	}
+	Else
+	{
+		Write-Warning "Cannot find network printer in the currently environment."
+	}
+}
+
+Remove-OSCNetworkPrinters
