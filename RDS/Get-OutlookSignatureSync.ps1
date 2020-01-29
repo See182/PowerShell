5d2ae04726ab6a201cb2d963e958320e98c1ab7a
Steve <steve.kirby@mailbox.org>
2020-01-29 15:00:10 +0100

    Variabeln definiert

--- RDS/Get-OutlookSignatureSync.ps1
+++ RDS/Get-OutlookSignatureSync.ps1
@@ -1,17 +1,20 @@
-$FolderRedirectionPath = "\\caritas.local\data\Profil\FolderRedirection\$($env:USERNAME)\Documents\Outlook-Dateien\Signatures"
+$AppDataFolder = "C:\Users\$($env:USERNAME)\AppData\Roaming\Microsoft\Signatures"
 
-# 1 Check if Signature Folder is in FolderRedirection
-$SignatureFolderRedirectionExist=Test-Path $FolderRedirectionPath
+$FolderRedirectionFolder = "\\caritas.local\data\Profil\FolderRedirection\$($env:USERNAME)\Documents\Outlook-Dateien\Signatures"
+$FolderRedirectionPath = "\\caritas.local\data\Profil\FolderRedirection\$($env:USERNAME)\Documents\Outlook-Dateien\"
+
+# 1 Check if Signature Folder is in FolderRedirection else Copy the Files in the AppData to the FolderRedirection
+$SignatureFolderRedirectionExist=Test-Path $FolderRedirectionFolder
 if ($SignatureFolderRedirectionExist) {
     # True
-    write-host "$($FolderRedirectionPath) Exists"
+    write-host "$($FolderRedirectionFolder) Exists"
 } else {
     # FalseChanges the LogDirectory to a local Folder 
-    write-host "$($FolderRedirectionPath) Does not exist fallback to a local dir"
-    Copy-Item -Path "C:\Users\$($env:USERNAME)\AppData\Roaming\Microsoft\Signatures" -Destination "\\caritas.local\data\Profil\FolderRedirection\$($env:USERNAME)\Documents\Outlook-Dateien\" -Recurse
+    write-host "$($FolderRedirectionFolder) Does not exist creating Folder"
+    Copy-Item -Path $AppDataFolder -Destination $FolderRedirectionPath -Recurse
 }
 
-## 2 Remove Signature Folder in AppData
-Remove-Item -Path "C:\Users\$($env:USERNAME)\AppData\Roaming\Microsoft\Signatures" -Force -Recurse
-## 3 Link Folder
-New-Item -ItemType SymbolicLink -Path "C:\Users\$($env:USERNAME)\AppData\Roaming\Microsoft\Signatures" -Value "\\caritas.local\data\Profil\FolderRedirection\$($env:USERNAME)\Documents\Outlook-Dateien\Signatures"
+# 2 Remove Signature Folder in AppData
+Remove-Item -Path $AppDataFolder -Force -Recurse
+# 3 Link Folder
+New-Item -ItemType SymbolicLink -Path $AppDataFolder -Value $FolderRedirectionFolder
