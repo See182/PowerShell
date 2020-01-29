a5f74aaa2122a10e5fce8a18b716488253a98452
Steve <steve.kirby@mailbox.org>
2020-01-29 17:46:45 +0100

    Same as OutlookSignature but for Mozillas Profiles

---
+++ RDS/Get-MozillaProfileSync.ps1
@@ -0,0 +1,39 @@
+$Share = "\\DOMAIN\data\Profil\FolderRedirection\$($env:USERNAME)"
+$Path = "Documents\Mozilla-Dateien"
+$Folder = "Mozilla"
+
+## Leave everything as it is
+$AppDataFolder = "C:\Users\$($env:USERNAME)\AppData\Roaming\Mozilla\"
+$FolderRedirectionPath = "$Share\$Path"
+$FolderRedirectionFolder = "$FolderRedirectionPath\$Folder"
+
+function Test-ReparsePoint([string]$path) {
+    $file = Get-Item $path -Force -ea SilentlyContinue
+    return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+  } # function found: https://stackoverflow.com/questions/817794/find-out-whether-a-file-is-a-symbolic-link-in-powershell
+
+# 1 Check if Signature Folder is in AppData Folder
+if (-Not (Test-Path $AppDataFolder)){
+    #Write-Host "No Signature Folder in AppData, I'll create one..."
+    New-Item -ItemType Directory -Path $AppDataFolder  
+}
+
+# 2 Check if Signature Folder is in FolderRedirection else Copy the Files in the AppData to the FolderRedirection
+if (-Not (Test-Path $FolderRedirectionFolder)) {
+    #Write-Host "Signatures Folder does not exist, copying Folder from AppData..."
+    Copy-Item -Path $AppDataFolder -Destination $FolderRedirectionPath -Recurse
+}
+
+# 3 Link Folder
+if (-Not (Test-ReparsePoint $AppDataFolder)){
+    #Write-Host "Removing Signatures Folder in AppData"
+    Remove-Item -Path $AppDataFolder -Force -Recurse 
+
+    #Write-Host "Linking AppData to FolderRedirection"
+    # This is why we need to execute as administrator
+    New-Item -ItemType SymbolicLink -Path $AppDataFolder -Value $FolderRedirectionFolder
+
+    #Enable this in case you dont have the newest Powershell 
+    # cmd /c "mklink /D C:\Users\%username%\AppData\Roaming\Microsoft\Signatures PATH\%username%\Documents\Outlook-Dateien\Signatures"
+
+}
