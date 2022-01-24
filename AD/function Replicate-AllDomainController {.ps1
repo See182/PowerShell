847af701fb9f3a5b606cd0450d7b7007ce198fc9
StKir <steve.kirby@concat.de>
2022-01-24 10:30:52 +0100

    DC FS

--- AD/function Replicate-AllDomainController {.ps1
+++ AD/function Replicate-AllDomainController {.ps1
@@ -1,5 +1,4 @@
 function Replicate-AllDomainController {
     (Get-ADDomainController -Filter *).Name | Foreach-Object {repadmin /syncall $_ (Get-ADDomain).DistinguishedName /e /A | Out-Null}; Start-Sleep 10; Get-ADReplicationPartnerMetadata -Target "$env:userdnsdomain" -Scope Domain | Select-Object Server, LastReplicationSuccess
     }
-    
-    Replicate-AllDomainController
+Replicate-AllDomainController
