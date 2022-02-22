5659f19d079196d45acd7b9fe6d5a271c7729f85
StKir <steve.kirby@concat.de>
2022-02-22 10:48:19 +0100

    ##

--- ServerManagement/StringReplacer.ps1
+++ ServerManagement/StringReplacer.ps1
@@ -2,4 +2,7 @@ $FindString = 'Get-GPUserCSE'
 $ReplaceString = 'Set-GPUserCSE'
 $Path = 'D:\temp'
 
-Get-ChildItem -Path $Path -Recurse | Replace($FindString, $ReplaceString)
+Get-ChildItem -Path $Path -Recurse | ForEach-Object{
+    $file = $_.FullName
+    (Get-Content $file) -replace $FindString, $ReplaceString | Set-Content $file
+}
--- ServerManagement/StringSearcher.ps1
+++ ServerManagement/StringSearcher.ps1
@@ -1,4 +1,4 @@
-$String = "Get-GPUserCSE"
+$String = "Set-GPUserCSE"
 $Path = 'D:\temp'
 
 Get-ChildItem -Path $Path -Recurse | Select-String -Pattern $String
